84019be641852dabe569a0894eecf2aa602d6ef6
joshua-gibson <joshua.gibson.dev@gmail.com>
2021-05-01 21:22:26 +0100

    dice rolling tweaked values

--- public/example.js
+++ public/example.js
@@ -37,15 +37,11 @@ const MainScene = () => {
   const physics = new AmmoPhysics(scene);
   physics.debug.enable(false);
 
-  // extract the object factory from physics
-  // the factory will make/add object without physics
-  const { factory } = physics;
-
-  // blue box
-  // physics.add.box({ x: 0.05, y: 10 }, { lambert: { color: 0x2194ce } });
-
   // static ground
-  physics.add.ground({ width: 120, height: 120 });
+  physics.add.ground(
+    { width: 120, height: 120 },
+    { lambert: { color: "rgb(0,100,0)" } }
+  );
 
   new THREE.GLTFLoader().loadAsync("die-alpha.gltf").then((gltf) => {
     const die = gltf.scene.children[2];
@@ -53,68 +49,40 @@ const MainScene = () => {
     die.position.y = 8;
     scene.add(die);
     physics.add.existing(die, { shape: "box", width: 2, height: 2, depth: 2 });
-    die.body.setGravity(0, -20, 0);
+    die.body.setGravity(0, -15, 0);
     die.body.setVelocity(3, 2, 20);
     die.body.setAngularVelocity(5, 7, 15);
+    die.body.setBounciness(1);
+    die.body.setFriction(0.6);
     // die.body.applyForceX(5);
     // die.body.applyForceZ(8);
     // die.body.applyForceY(0);
     // die.body.applyTorque(0, 15, 15);
   });
 
-  // add a normal sphere using the object factory
-  // (NOTE: This will be factory.add.sphere() in the future)
-  // first parameter is the config for the geometry
-  // second parameter is for the material
-  // you could also add a custom material like so { custom: new THREE.MeshLambertMaterial({ color: 0x00ff00 }) }
-  // let greenSphere = factory.addSphere(
-  //   { y: 2, z: 5 },
-  //   { lambert: { color: 0x00ff00 } }
-  // );
-  // once the object is created, you can add physics to it
-  // physics.add.existing(greenSphere);
-
-  // green sphere
-  // const geometry = new THREE.BoxBufferGeometry();
-  // const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-  // const cube = new THREE.Mesh(geometry, material);
-  // cube.position.set(0, 5, 0);
-  // scene.add(cube);
-  // physics.add.existing(cube);
-  // cube.body.setCollisionFlags(2); // make it kinematic
-
-  // merge children to compound shape
-  // const exclamationMark = () => {
-  //   const material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
-
-  //   const sphere = new THREE.Mesh(
-  //     new THREE.SphereBufferGeometry(0.25),
-  //     material
-  //   );
-  //   sphere.position.set(0, -0.8, 0);
-
-  //   const cube = new THREE.Mesh(
-  //     new THREE.BoxBufferGeometry(0.4, 0.8, 0.4),
-  //     material
-  //   );
-  //   cube.position.set(5, 2, 5);
-
-  //   cube.add(sphere);
-  //   scene.add(cube);
-
-  //   cube.position.set(5, 5, 5);
-  //   cube.rotation.set(0, 0.4, 0.2);
-
-  //   physics.add.existing(cube);
-  // };
-  // exclamationMark();
+  new THREE.GLTFLoader().loadAsync("die-alpha.gltf").then((gltf) => {
+    const die_two = gltf.scene.children[2];
+    die_two.position.z = -52;
+    die_two.position.y = 7;
+    scene.add(die_two);
+    physics.add.existing(die_two, {
+      shape: "box",
+      width: 2,
+      height: 2,
+      depth: 2,
+    });
+    die_two.body.setGravity(0, -15, 0);
+    die_two.body.setVelocity(2, 3, 17);
+    die_two.body.setAngularVelocity(2, 8, 15);
+    die_two.body.setFriction(0.5);
+  });
 
   // clock
   const clock = new THREE.Clock();
 
   // loop
   const animate = () => {
-    physics.update(clock.getDelta() * 1000);
+    physics.update(clock.getDelta() * 10000);
 
     renderer.render(scene, camera);
 
