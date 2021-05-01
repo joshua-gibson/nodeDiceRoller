882d4e946f2a07fa7ad1ffc3cd058890981ec307
joshua-gibson <joshua.gibson.dev@gmail.com>
2021-05-01 20:53:17 +0100

    dice rolling

--- public/example.js
+++ public/example.js
@@ -1,4 +1,4 @@
-const { AmmoPhysics, PhysicsLoader } = ENABLE3D;
+const { AmmoPhysics, PhysicsLoader, ExtendedObject3D } = ENABLE3D;
 
 const MainScene = () => {
   // scene
@@ -12,7 +12,7 @@ const MainScene = () => {
     0.1,
     1000
   );
-  camera.position.set(10, 10, 20);
+  camera.position.set(40, 60, 40);
 
   // renderer
   const renderer = new THREE.WebGLRenderer();
@@ -35,76 +35,87 @@ const MainScene = () => {
 
   // physics
   const physics = new AmmoPhysics(scene);
-  physics.debug.enable(true);
+  physics.debug.enable(false);
 
   // extract the object factory from physics
   // the factory will make/add object without physics
   const { factory } = physics;
 
   // blue box
-  physics.add.box({ x: 0.05, y: 10 }, { lambert: { color: 0x2194ce } });
+  // physics.add.box({ x: 0.05, y: 10 }, { lambert: { color: 0x2194ce } });
 
   // static ground
-  physics.add.ground({ width: 20, height: 20 });
+  physics.add.ground({ width: 120, height: 120 });
+
+  new THREE.GLTFLoader().loadAsync("die-alpha.gltf").then((gltf) => {
+    const die = gltf.scene.children[2];
+    die.position.z = -55;
+    die.position.y = 8;
+    scene.add(die);
+    physics.add.existing(die, { shape: "box", width: 2, height: 2, depth: 2 });
+    die.body.setGravity(0, -20, 0);
+    die.body.setVelocity(3, 2, 20);
+    die.body.setAngularVelocity(5, 7, 15);
+    // die.body.applyForceX(5);
+    // die.body.applyForceZ(8);
+    // die.body.applyForceY(0);
+    // die.body.applyTorque(0, 15, 15);
+  });
 
   // add a normal sphere using the object factory
   // (NOTE: This will be factory.add.sphere() in the future)
   // first parameter is the config for the geometry
   // second parameter is for the material
   // you could also add a custom material like so { custom: new THREE.MeshLambertMaterial({ color: 0x00ff00 }) }
-  let greenSphere = factory.addSphere(
-    { y: 2, z: 5 },
-    { lambert: { color: 0x00ff00 } }
-  );
+  // let greenSphere = factory.addSphere(
+  //   { y: 2, z: 5 },
+  //   { lambert: { color: 0x00ff00 } }
+  // );
   // once the object is created, you can add physics to it
-  physics.add.existing(greenSphere);
+  // physics.add.existing(greenSphere);
 
   // green sphere
-  const geometry = new THREE.BoxBufferGeometry();
-  const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  cube.position.set(0, 5, 0);
-  scene.add(cube);
-  physics.add.existing(cube);
-  cube.body.setCollisionFlags(2); // make it kinematic
+  // const geometry = new THREE.BoxBufferGeometry();
+  // const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+  // const cube = new THREE.Mesh(geometry, material);
+  // cube.position.set(0, 5, 0);
+  // scene.add(cube);
+  // physics.add.existing(cube);
+  // cube.body.setCollisionFlags(2); // make it kinematic
 
   // merge children to compound shape
-  const exclamationMark = () => {
-    const material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
+  // const exclamationMark = () => {
+  //   const material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
 
-    const sphere = new THREE.Mesh(
-      new THREE.SphereBufferGeometry(0.25),
-      material
-    );
-    sphere.position.set(0, -0.8, 0);
+  //   const sphere = new THREE.Mesh(
+  //     new THREE.SphereBufferGeometry(0.25),
+  //     material
+  //   );
+  //   sphere.position.set(0, -0.8, 0);
 
-    const cube = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(0.4, 0.8, 0.4),
-      material
-    );
-    cube.position.set(5, 2, 5);
+  //   const cube = new THREE.Mesh(
+  //     new THREE.BoxBufferGeometry(0.4, 0.8, 0.4),
+  //     material
+  //   );
+  //   cube.position.set(5, 2, 5);
 
-    cube.add(sphere);
-    scene.add(cube);
+  //   cube.add(sphere);
+  //   scene.add(cube);
 
-    cube.position.set(5, 5, 5);
-    cube.rotation.set(0, 0.4, 0.2);
+  //   cube.position.set(5, 5, 5);
+  //   cube.rotation.set(0, 0.4, 0.2);
 
-    physics.add.existing(cube);
-  };
-  exclamationMark();
+  //   physics.add.existing(cube);
+  // };
+  // exclamationMark();
 
   // clock
   const clock = new THREE.Clock();
 
   // loop
   const animate = () => {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.body.needUpdate = true; // this is how you update kinematic bodies
-
     physics.update(clock.getDelta() * 1000);
-    physics.updateDebugger();
+
     renderer.render(scene, camera);
 
     requestAnimationFrame(animate);
@@ -112,5 +123,3 @@ const MainScene = () => {
   requestAnimationFrame(animate);
 };
 PhysicsLoader("/lib/ammo/kripken", () => MainScene());
-
-console.log(`three.js version "${THREE.REVISION}"`);
