3e6158a4bb21ae2529fca07fd3cf943bb34a1a26
joshua-gibson <joshua.gibson.dev@gmail.com>
2021-05-01 14:18:55 +0100

    physics example working

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/nm/", express.static(path.join(__dirname, "node_modules")));
 
+app.use(
+  "/lib/",
+  express.static(path.join(__dirname, "node_modules/@enable3d/lib"))
+);
+
 app.listen(8080, () => console.log("Visit http://127.0.0.1:8080"));
